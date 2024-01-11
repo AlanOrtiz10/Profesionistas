f19a323d2a87ba9dd340580815362a510147cfc1
Alan <a22311050@uthermosillo.edu.mx>
2024-01-11 08:37:53 -0700

    Se crearon las migraciones

---
+++ api/profesionistas/database/migrations/2024_01_11_031701_create_specialist_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecialistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specialist', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->string('image');
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specialist');
+    }
+}
